ed49c6e4be6d2548333335a97657a5bae215d986
Jake Chung <luonchung@gmail.com>
2020-02-29 01:13:08 -0800

    change res

--- Code/Matlab/makePoints.m
+++ Code/Matlab/makePoints.m
@@ -8,7 +8,7 @@ function[list] = makePoints(matrix)
 % plot(matrix(:,1),matrix(:,2),'o')
 % xlim([-1 40])
 % ylim([-1 45])
-np=10;
+np=20;
 list = zeros(1,3);
 i=1;
 while (i ~= (length(matrix(:,1))))
@@ -43,4 +43,6 @@ end
 %clean up list
 list = list(2:end,:);
 list(end,:) = matrix(end,:);
+% %turn on plot function to check makePoints
+% plot(list(:,1), list(:,3),'o-')
 end
