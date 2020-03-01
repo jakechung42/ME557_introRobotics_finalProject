4d22a23907a46065d73895550a7368bbdfd9b083
Jake Chung <luonchung@gmail.com>
2020-03-01 12:48:27 -0800

    add comments and move the liftPen function to become a global function

--- Code/Matlab/pathGen.m
+++ Code/Matlab/pathGen.m
@@ -90,44 +90,6 @@ thetaList = thetaList(2:end,:);
 thetaList = angLim(thetaList);
 %end
 
-%% this function generate the trajectory for the lift pen action.
-%This function is called when there's a -999 value detected.
-function[out] = liftPen(p1, p2, preTheta, Slist, M)
-%p1 is the point on the board 
-%p2 is the next point that the tip of then pen needs to move to
-%lift the pen from the surface of the whiteboard
-p1(1,2) = p1(1,2)-10; %move away from the board some amount
-L = norm(p2-p1); %get the length of the vector to move to the next vertex
-uv = (p2-p1)/L; %unit vector
-np = 5; %number of points to get to p2 increase this number if life pen start moving chaotically
-dL = L/np;
-ew = 1;
-ev = 0.1;
-out = [0, 0, 0, 0, 0];
-%first the tip of the pen has to move to the new p1 off of the whiteboard
-[path1st, sucess] = IKinSpace(Slist, M, buildT(p1), preTheta', ew, ev);
-if sucess == 0
-    fprintf('Fail to make path for intial point for lifting pen!\n')
-    return;
-else 
-    preTheta = path1st;
-end
-%start inverse kinematics for the rest of the path to the next point
-for i = 1:np
-    stp = p1+i*uv*dL;
-    [path, sucess] = IKinSpace(Slist, M, buildT(stp), preTheta, ew, ev);
-    if sucess == 0
-        fprintf('Fail to make path for lift pen!\n')
-        return;
-    end
-    path = path';
-    out(end+1,:) = path;
-    preTheta = path'; %give new initial guess
-end
-out = out(2:end,:);
-out = [path1st';out];
-end
-
 %% This function builds the T matrix using the "standard" orientation and the input translation vector
 %the rotation matrix is R = [1 0 0; 0 0 -1; 0 1 0]
 function[T_out] = buildT(vec)
