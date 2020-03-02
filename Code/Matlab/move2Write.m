c75c8c02619a2117819647eb87c2af4f2c4d53ad
Jake Chung <luonchung@gmail.com>
2020-03-02 10:35:02 -0800

    Create move2Write.m

---
+++ Code/Matlab/move2Write.m
@@ -0,0 +1,25 @@
+function[] = move2Write(s)
+%this function moves the arm to the write position from the home position. Should be called before
+%starting to write 
+%Author: Jake Chung
+%Winter 2020
+%Final project Intro to Robotics.
+
+%bring arm to home
+homeAll(s)
+pause(1);
+
+%set motors 4 5 6 first.
+posSet(s, 6, 610);
+posSet(s, 5, 663);
+posSet(s, 4, 281);
+
+%motor 1 2 3 will go in steps
+n_step = 20;
+for i= 1:n_step
+    posSet(s,3,i*(619-512)/n_step+512);
+    posSet(s,2,2048-i*(2048-1584)/n_step);
+    posSet(s,1,i*(2788-2048)/n_step+2048);
+    pause(0.3);
+end
+end
