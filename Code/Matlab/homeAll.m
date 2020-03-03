af07c854591d19451e76650bebf644415d13bb0a
Jake Chung <luonchung@gmail.com>
2020-03-02 17:12:15 -0800

    Update homeAll.m

--- Code/Matlab/homeAll.m
+++ Code/Matlab/homeAll.m
@@ -2,13 +2,13 @@ function[] = homeAll(s)
 %home all motors
 
 for i = 3:6
-    temp = posSet(s, i, 512);
+    posSet(s, i, 512);
     pause(0.2);
 end
 
 for i = 1:2
-    temp = posSet(s, i, 2048);
-    pause(0.3);
+    posSet(s, i, 2048);
+    pause(0.7);
 end
 
 end
