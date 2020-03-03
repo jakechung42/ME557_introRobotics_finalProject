f3a534cc7779b927a37ee67099767922b0f71153
Jake Chung <luonchung@gmail.com>
2020-03-03 15:36:58 -0800

    tune timing to the extremem

--- Code/Matlab/posAsk.m
+++ Code/Matlab/posAsk.m
@@ -3,6 +3,7 @@ function[angle] = posAsk(serial, id)
 %the input is the serial and the motor id that is be asked for
 %prepare the "id" byte to send to OpenCM9.04
 sendByte = id+8;
+pause(3)
 fwrite(serial, sendByte);
 %read the returned angular position
 angle = (fread(serial, 1) + fread(serial, 1)*256);
