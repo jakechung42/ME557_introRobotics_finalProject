a505a7e54bd3f1fea98b4ec54261c7fe8f0cccbc
Jake Chung <luonchung@gmail.com>
2020-03-03 18:35:05 -0800

    imrpove homeAll

--- Code/Matlab/homeAll.m
+++ Code/Matlab/homeAll.m
@@ -1,16 +1,16 @@
 function[] = homeAll(s)
 %home all motors
-
+fprintf('Home 2\n')
+pause(1)
+posSet(s,2,2048);
+pause(1)
 for i = 3:6
     fprintf('Home %d\n',i);
     posSet(s, i, 512);
-    pause(0.5);
+    pause(0.8);
 end
-
-for i = 1:2
-    fprintf('Home %d\n',i);
-    posSet(s, i, 2048);
-    pause(2);
-end
+pause(1)
+fprintf('Home 1\n')
+posSet(s,1,2048)
 fprintf('Done home\n')
 end
