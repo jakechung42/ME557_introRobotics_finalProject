083649a7d986e47cb0aa8a6f9d2f0a987d5c9720
Jake Chung <luonchung@gmail.com>
2020-03-03 16:36:28 -0800

    Update posSet.m

--- Code/Matlab/posSet.m
+++ Code/Matlab/posSet.m
@@ -1,6 +1,7 @@
 %this function send a position to the serial port to move the dynamexials
 function[] = posSet(serial, id, goalPos)
     %read decompose the desire position to send via serial
+    goalPos = round(goalPos);
     low = mod(goalPos, 256);
     high = floor(goalPos/256);
     %write the data to serial
