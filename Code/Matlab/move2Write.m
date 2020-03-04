7b8b0f07e3172400b98c72b7e25ecff6ac9402a1
Jake Chung <luonchung@gmail.com>
2020-03-03 16:36:22 -0800

    Update move2Write.m

--- Code/Matlab/move2Write.m
+++ Code/Matlab/move2Write.m
@@ -17,12 +17,14 @@ pause(1)
 posSet(s, 4, 281);
 pause(1)
 %motor 1 2 3 will go in steps
-n_step = 20;
+n_step = 8;
 for i= 1:n_step
     posSet(s,3,i*(619-512)/n_step+512);
+    pause(0.2);
     posSet(s,2,2048-i*(2048-1584)/n_step);
+    pause(0.2);
     posSet(s,1,i*(2788-2048)/n_step+2048);
-    pause(0.6);
+    pause(0.2);
 end
 posSet(s,3,619);
 pause(0.5)
